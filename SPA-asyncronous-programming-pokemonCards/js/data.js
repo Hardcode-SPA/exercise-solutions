58ae67353a91c74767784efd9ea8c37d8dca3a3f
Alex <babel.softwaresolutions-freelance@digitalcareerinstitute.org>
2022-11-24 12:23:21 +0100

    more or less completed SPA-asyncronous-programming-pokemonCards

--- SPA-asyncronous-programming-pokemonCards/js/data.js
+++ SPA-asyncronous-programming-pokemonCards/js/data.js
@@ -1,15 +1,50 @@
+let allPokemon = [];
 
-
-
-async function fetchByName(name) {
+async function fetchAll() {
     try {
+        let response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=-1');
+
+        // Wenn Antwort-Status 404 ist, werfe Fehler fuer nicht gefunden
+        if ( response.status === 404 ) throw new Error('Not found');
+        // Wenn Antwort-Status nicht 200 ist, werfe allgemeinen Fehler
+        if ( !response.ok ) throw new Error('Error occured');
+
+        // Wandle erhaltenen Datensatz in JS-Objekt um
+        let body = await response.json();
+
+        console.log(body.results);
+
+        allPokemon = body.results;
+    } catch (error) {
+        
+    }
+}
+
+function findAllMatchingByName(query) {
+    if (query.trim().length === 0) return [];
+    
+    return allPokemon.filter(pokemon => {
+        return pokemon.name.toLowerCase().includes(query.toLowerCase());
+    });
+}
+
+// Funktion zum Abrufen eines Pokemon-Datensatzes von der PokeAPI
+async function fetchByName(name) {
+    // Versuche
+    try {
+        // Fetch auf die pokemon-Route mit dem uebergebenen Suchstring
         let response = await fetch('https://pokeapi.co/api/v2/pokemon/' + name);
         
-        if ( !response.ok ) throw new Error('Not found');
+        // Wenn Antwort-Status 404 ist, werfe Fehler fuer nicht gefunden
+        if ( response.status === 404 ) throw new Error('Not found');
+        // Wenn Antwort-Status nicht 200 ist, werfe allgemeinen Fehler
+        if ( !response.ok ) throw new Error('Error occured');
         
+        // Wandle erhaltenen Datensatz in JS-Objekt um
         let body = await response.json();
         console.log(body);
 
+        // Gebe custom Pokemon Objekt zurueck
         return {
             name: body.name,
             img: body.sprites.front_default,
@@ -19,15 +54,17 @@ async function fetchByName(name) {
                     score: stat.base_stat
                 };
             }),
-            abbilities: body.abilities.map(ability => {
+            abilities: body.abilities.map(ability => {
                 return ability.ability.name
             })
         };
-    } catch (error) {
+
+    } catch (error) { // Fange Fehler ab
         console.log(error);
 
+        // Gebe null zurueck, sodass Aufrufer weiss, dass Fehler entstanden ist
         return null;
     }
 }
 
-export { fetchByName };
+export { fetchByName, fetchAll, findAllMatchingByName };
--- SPA-asyncronous-programming-pokemonCards/js/index.js
+++ SPA-asyncronous-programming-pokemonCards/js/index.js
@@ -1,9 +1,175 @@
-import { fetchByName } from './data.js';
+import { fetchByName, fetchAll, findAllMatchingByName } from './data.js';
 
-// let pokeData = await fetchByName('pikachu');
-let pokeData = await fetchByName('peter pan');
+// Hole alle Pokemon Eintraege in Kurzform
+fetchAll();
 
-if ( !pokeData ) {
-    new bootstrap.Toast(document.querySelector('.toast')).show();
+// Hole Suchformular
+let searchForm = document.querySelector('#search-pokemon-form');
+// Haenge submit-Handler an Eingabefeld an
+searchForm.addEventListener('submit', handleSearchSubmit);
+// Hole Suchvorschlagsliste
+let suggestions = searchForm.querySelector('ul');
+
+// Haenge input-Handler an Eingabefeld an
+searchForm.addEventListener('input', evt => {
+    let query = evt.target.value.trim();
+
+    let results = findAllMatchingByName(query);
+
+    console.log(findAllMatchingByName(query));
+
+    // Mache aus den ersten 10 Eintraegen list items
+    let suggestionItems = results.slice(0, 9).map(result => {
+        let listItem = document.createElement('li');
+        listItem.classList.add('list-group-item', 'list-group-item-action');
+        listItem.textContent = capitalize(result.name);
+        listItem.addEventListener('click', evt => {
+            performSearch(result.name);
+        });
+        return listItem;
+    });
+
+    console.log(suggestions);
+
+    suggestions.replaceChildren(...suggestionItems);
+});
+
+
+async function performSearch(searchQuery) {
+    // Hole Spinner und zeige an
+    let spinner = document.querySelector('#query-spinner');
+    spinner.classList.toggle('visually-hidden');
+
+    // Hole Pokemon Card und verstecke ggf.
+    let pokemonCard = document.querySelector('#pokemon-card');
+    if (!pokemonCard.classList.contains('visually-hidden')) {
+        pokemonCard.classList.toggle('visually-hidden');
+    }
+
+    // Starte und erwarte Suchanfrage
+    let pokeData = await fetchByName(searchQuery);
+
+    // Verstecke Spinner wieder
+    spinner.classList.toggle('visually-hidden');
+
+    // Wenn Suchanfrage null liefert
+    if ( !pokeData ) {
+        // Zeige Fehlertoast an
+        let newToast = createToast('Error', 'No such pokemon found!');
+        document.querySelector('.toast-container').appendChild(newToast);
+        new bootstrap.Toast(newToast).show();
+
+        // Early return
+        return;
+    }
+
+    // Setze Suchformular zurueck
+    resetSearchForm();
+
+    // Daten sind da -> Befuelle Pokemon Card und zeige sie an
+    fillPokeCard(pokeData);
+    pokemonCard.classList.toggle('visually-hidden');
 }
-console.log(pokeData);
+
+async function handleSearchSubmit(evt) {
+    evt.preventDefault();
+
+    // Extrahiere Suchbegriff
+    let searchQuery = evt.target.querySelector('input').value.toLowerCase();
+    // Pruefe, ob Suchbegriff gueltig
+    if (searchQuery.trim().length <= 0) {
+        // Zeige Fehler Toast an
+        let newToast = createToast('Error', 'Your query was empty...');
+        document.querySelector('.toast-container').appendChild(newToast);
+        new bootstrap.Toast(newToast).show();
+
+        // early return
+        return;
+    }
+
+    // Fuehre den Suchvorgang durch
+    performSearch(searchQuery);
+}
+
+function fillPokeCard(pokeData) {
+    let pokemonCard = document.querySelector('#pokemon-card');
+
+    let pokemonTitle = pokemonCard.querySelector('h3');
+    pokemonTitle.textContent = pokeData.name;
+    
+    let pokemonImg = pokemonCard.querySelector('img');
+    pokemonImg.alt = pokeData.name;
+    pokemonImg.src = pokeData.img;
+
+    let pokemonStats = pokemonCard.querySelector('#poke-stats');
+    pokemonStats.replaceChildren();
+    pokeData.stats.forEach(stat => {
+        let statItem = document.createElement('li');
+        statItem.classList.add('list-group-item', 'bg-dark', 'text-light');
+        statItem.textContent = `${stat.name}: ${stat.score}`;
+        pokemonStats.appendChild(statItem);
+    });
+
+    let pokemonAbilities = pokemonCard.querySelector('#poke-abilities');
+    pokemonAbilities.replaceChildren();
+    pokeData.abilities.forEach(ability => {
+        let abilityItem = document.createElement('li');
+        abilityItem.classList.add('list-group-item', 'bg-dark', 'text-light');
+        abilityItem.textContent = ability;
+        pokemonAbilities.appendChild(abilityItem);
+    });
+}
+
+function createToast(title, message) {
+    
+    let toast = document.createElement('div');
+    toast.classList.add('toast');
+    toast.setAttribute('role', 'alert');
+    toast.setAttribute('aria-live', 'assertive');
+    toast.setAttribute('aria-atomic', 'true');
+    
+    let toastHeader = document.createElement('div');
+    toastHeader.classList.add('toast-header');
+    toast.appendChild(toastHeader);
+
+    let toastIcon = document.createElement('div');
+    toastIcon.classList.add('me-1');
+    toastIcon.style.width = '20px';
+    toastIcon.style.height = '20px';
+    toastIcon.style.backgroundColor = '#ff0000BB';
+    toastIcon.style.borderRadius = '5px';
+    toastHeader.appendChild(toastIcon);
+
+    let toastTitle = document.createElement('strong');
+    toastTitle.classList.add('me-auto');
+    toastTitle.textContent = title;
+    toastHeader.appendChild(toastTitle);
+
+    let toastTimestamp = document.createElement('small');
+    toastTimestamp.classList.add('text-muted');
+    toastTimestamp.textContent = 'just now';
+    toastHeader.appendChild(toastTimestamp);
+
+    let toastCloseBtn = document.createElement('button');
+    toastCloseBtn.classList.add('btn-close');
+    toastCloseBtn.setAttribute('data-bs-dismiss', 'toast');
+    toastCloseBtn.setAttribute('aria-label', 'Close');
+    toastHeader.appendChild(toastCloseBtn);
+
+    let toastBody = document.createElement('div');
+    toastBody.classList.add('toast-body');
+    toastBody.textContent = message;
+    toast.appendChild(toastBody);
+
+    return toast;
+}
+
+
+function resetSearchForm() {
+    searchForm.querySelector('input').value = '';
+    suggestions.replaceChildren();
+}
+
+function capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
